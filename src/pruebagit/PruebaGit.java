262de7e9305dd5e942551e8865043e447e70bb42
Ruben <Ruben@DESKTOP-2EIGM9G>
2020-01-14 20:10:13 +0100

    prueba de commit añadido metodo quita

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -10,12 +10,20 @@ package pruebagit;
  * @author Ruben
  */
 public class PruebaGit {
-
+private static void pedro(){
+        String ruben="Hola mundo";
+       int tamanio=ruben.length();
+       System.out.println("tamaño del texto es "+tamanio);
+              System.out.println("la cadena sin espacios queda asi ".concat(ruben.replace(" ", "")));
+       System.out.println("tamaño del texto sin espacios es "+ruben.replace(" ", "").length());
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba GIT");
+        
+        //https://github.com/rubenjuarez1986/pruebaGit.git
+        //he añadido un metodo que quita espacios
     }
     
 }
